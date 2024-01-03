a000d90803c4b19988d696397e90b683401bc249
Nilesssssh <155536454+Nilesssssh@users.noreply.github.com>
2024-01-03 19:38:43 +0000

    container config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144457
+let accountEmail = "nilesh15415@gmail.com"
+var accountPasswordv = "124560"
+accountCity = "jaipur"
+let accountState;
+
+//accountId = 2//not allowed
+
+/*
+prefer not to use var
+because of issue in block scope and function scope
+*/
+
+
+
+console.log(accountId);
